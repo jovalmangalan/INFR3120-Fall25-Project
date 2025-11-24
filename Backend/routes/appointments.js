9a12e50cf42442369387a77aad9af2d5c2606a3e
Joval Mangalan <joval.mangalan@ontariotechu.net>
2025-11-24 14:20:48 -0500

    Added appointment routes to make sure edits/deletes are only for login users

--- Backend/routes/appointments.js
+++ Backend/routes/appointments.js
@@ -1,8 +1,10 @@
 import express from "express";
 import controller from "../controllers/appointmentsController.js";
+import { requireAuth } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+// PUBLIC PAGES
 router.get("/", controller.showHome);
 
 router.get("/schedule", controller.showSchedulePage);
@@ -10,9 +12,10 @@ router.post("/schedule", controller.createAppointment);
 
 router.get("/calendar", controller.showCalendar);
 
-router.get("/edit/:id", controller.showEditPage);
-router.post("/edit/:id", controller.updateAppointment);
+// PROTECTED ROUTES (must be logged in)
+router.get("/edit/:id", requireAuth, controller.showEditPage);
+router.post("/edit/:id", requireAuth, controller.updateAppointment);
 
-router.get("/delete/:id", controller.deleteAppointment);
+router.get("/delete/:id", requireAuth, controller.deleteAppointment);
 
 export default router;
